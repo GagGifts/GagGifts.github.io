1a469e43a363b6cf6529251e5c4ad9c32bf26bfa
Matthew Sheldon <punchwood2003@gmail.com>
2023-04-11 22:15:45 -0500

    Updates

---
+++ precache-manifest.d025af1b25281efd02f0370859dc40c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e47706c4a8576bc2ff420c744b1e44d5",
+    "url": "/public/index.html"
+  },
+  {
+    "revision": "0fe5e3778b34c0acd057",
+    "url": "/public/static/css/main.903ebc1a.chunk.css"
+  },
+  {
+    "revision": "0c6b01d513ad2f585506",
+    "url": "/public/static/js/2.e5bb400d.chunk.js"
+  },
+  {
+    "revision": "0fe5e3778b34c0acd057",
+    "url": "/public/static/js/main.24e8762b.chunk.js"
+  },
+  {
+    "revision": "71839442e14b9da6d00a",
+    "url": "/public/static/js/runtime~main.de5d37ed.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/public/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/public/static/media/shopping-bag.30f07071.svg"
+  }
+]);
