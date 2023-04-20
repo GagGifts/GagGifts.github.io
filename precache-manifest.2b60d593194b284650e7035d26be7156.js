f8112fd75bbbeae7678dfb9e4e81735c51ee4519
boyperfer <boyperfer@gmail.com>
2023-04-20 16:20:40 -0500

    Updates

---
+++ precache-manifest.2b60d593194b284650e7035d26be7156.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a593cf49f676e963102fef2dcb1ae57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf68bbf6aa88dcc3eca",
+    "url": "/static/css/main.2581971d.chunk.css"
+  },
+  {
+    "revision": "094d93e4008c65b6e40d",
+    "url": "/static/js/2.4f5f0730.chunk.js"
+  },
+  {
+    "revision": "fbf68bbf6aa88dcc3eca",
+    "url": "/static/js/main.1346e74d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47e9e7380814f7085190cd33171d1a2d",
+    "url": "/static/media/GagGiftsLogo.47e9e738.svg"
+  },
+  {
+    "revision": "187a59fcac41e5a2cf4cb6a68cb0cabf",
+    "url": "/static/media/logo.187a59fc.png"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//gaggifts.github.io/precache-manifest.13dfa645be2fdf040c5889253978dbc0.js"
+  "/precache-manifest.2b60d593194b284650e7035d26be7156.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//gaggifts.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
